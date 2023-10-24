58eca4aef4ad33b6bb2ccd953e654c458ded3024
William Herr <herrwilliam@outlook.com>
2023-10-23 21:25:12 -0400

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/", "/api/webbhook/clerk"],
-  ignoredRoutes: ["/api/webbhook/clerk"],
+  publicRoutes: ["/", "/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"],
   //debug: true,
 });
 
